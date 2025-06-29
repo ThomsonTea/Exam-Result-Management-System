fab32a842ca7665c01bc96e624dfd23a696466a1
soonkit-king <kitking2002@gmail.com>
2025-06-29 22:19:10 +0800

    Update erms_db_v1.sql

--- erms_db_v1.sql
+++ erms_db_v1.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `mark`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `mark` (
-  `markID` int NOT NULL,
+  `markID` int NOT NULL AUTO_INCREMENT,
   `studentID` varchar(50) DEFAULT NULL,
   `subjectID` varchar(50) DEFAULT NULL,
   `teacherID` varchar(50) DEFAULT NULL,
@@ -128,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-29 22:08:15
+-- Dump completed on 2025-06-29 22:18:57
